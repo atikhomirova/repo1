0e982c8fa57ae10cea4149e318b2cd8b12ef74f2
ant <3968204@gmail.com>
2018-09-23 15:25:23 +0300

    Short review.

--- task01.py
+++ task01.py
@@ -1,6 +1,21 @@
 # Task 01
 # Find most frequent element in the list
 
+"""
+Anton:
+Please take a look at PEP8.
+Also I highly recommend you use the following tools to check code style:
+- flake8;
+- pydocstyle.
+"""
+
 l = [1, 2, 3, 3, 3, 4, 5, 5, 5, 5]
 
 print(max(set(l),key=l.count))
+
+"""
+Anton:
+Good practice is to use the following construction:
+if __name__ == '__main__':
+    run your code
+"""
