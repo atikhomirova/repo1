23a97926171bf0ef630fbac263641994d5fe1e03
atikhomirova <an.tikhomirovaa@gmail.com>
2018-10-02 14:33:14 +0300

    correction work

--- task01.py
+++ task01.py
@@ -9,9 +9,9 @@ Also I highly recommend you use the following tools to check code style:
 - pydocstyle.
 """
 
-l = [1, 2, 3, 3, 3, 4, 5, 5, 5, 5]
-
-print(max(set(l),key=l.count))
+if __name__ == "__main__":
+    lst = [1, 2, 3, 3, 3, 4, 5, 5, 5, 5]
+    print(max(set(lst), key=lst.count))
 
 """
 Anton:
